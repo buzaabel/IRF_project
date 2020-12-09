a9967e8752f8cccfe3d16e2dfd969c6488016ce7
ABEL BUZA <buza.abel@gmail.com>
2020-12-09 15:09:28 +0100

    commit: fajlba iras LearningCards

--- ivok11_IRF_Project/ivok11_IRF_Project/LearningCardsForm.Designer.cs
+++ ivok11_IRF_Project/ivok11_IRF_Project/LearningCardsForm.Designer.cs
@@ -30,23 +30,35 @@ namespace ivok11_IRF_Project
         private void InitializeComponent()
         {
             this.NewCardBtn = new System.Windows.Forms.Button();
+            this.filewritingbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // NewCardBtn
             // 
-            this.NewCardBtn.Location = new System.Drawing.Point(41, 25);
+            this.NewCardBtn.Location = new System.Drawing.Point(292, 131);
             this.NewCardBtn.Name = "NewCardBtn";
-            this.NewCardBtn.Size = new System.Drawing.Size(153, 38);
+            this.NewCardBtn.Size = new System.Drawing.Size(179, 75);
             this.NewCardBtn.TabIndex = 0;
             this.NewCardBtn.Text = "New card";
             this.NewCardBtn.UseVisualStyleBackColor = true;
             this.NewCardBtn.Click += new System.EventHandler(this.NewCardBtn_Click);
+            // 
+            // filewritingbtn
+            // 
+            this.filewritingbtn.Location = new System.Drawing.Point(292, 224);
+            this.filewritingbtn.Name = "filewritingbtn";
+            this.filewritingbtn.Size = new System.Drawing.Size(178, 31);
+            this.filewritingbtn.TabIndex = 1;
+            this.filewritingbtn.Text = "Write cards in file";
+            this.filewritingbtn.UseVisualStyleBackColor = true;
+            this.filewritingbtn.Click += new System.EventHandler(this.filewritingbtn_Click);
             // 
             // LearningCardsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.filewritingbtn);
             this.Controls.Add(this.NewCardBtn);
             this.Name = "LearningCardsForm";
             this.Text = "LearningCardsForm";
@@ -57,5 +69,6 @@ namespace ivok11_IRF_Project
         #endregion
 
         private System.Windows.Forms.Button NewCardBtn;
+        private System.Windows.Forms.Button filewritingbtn;
     }
 }
--- ivok11_IRF_Project/ivok11_IRF_Project/LearningCardsForm.cs
+++ ivok11_IRF_Project/ivok11_IRF_Project/LearningCardsForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -82,6 +83,34 @@ namespace ivok11_IRF_Project
             szam = rnd.Next(0, x);
             card = new MovingCards();
             card.Text = carslist[szam].Name + " " + carslist[szam].Model + ": " + carslist[szam].Price.ToString();
+        }
+
+        private void filewritingbtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath; 
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";  
+            sfd.DefaultExt = "csv"; 
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                
+                foreach (var el in ellenor)
+                {                    
+                    sw.Write(el.Name);
+                    sw.Write(";");
+                    sw.Write(el.Model);
+                    sw.Write(";");
+                    sw.Write(el.Color);
+                    sw.Write(";");
+                    sw.Write(el.Price.ToString());                  
+                    sw.WriteLine(); 
+                }
+            }
         }
     }
 }
