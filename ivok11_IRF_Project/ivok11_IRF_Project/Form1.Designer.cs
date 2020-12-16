99e926cbbd446d5532fec34b6c17d2c29f7fc22d
ABEL BUZA <buza.abel@gmail.com>
2020-12-16 16:51:05 +0100

    commit kész program

--- ivok11_IRF_Project/ivok11_IRF_Project/Form1.Designer.cs
+++ ivok11_IRF_Project/ivok11_IRF_Project/Form1.Designer.cs
@@ -32,20 +32,21 @@ namespace ivok11_IRF_Project
             this.LCardsBtn = new System.Windows.Forms.Button();
             this.GuessGamebtn = new System.Windows.Forms.Button();
             this.game1btn = new System.Windows.Forms.Button();
-            this.design14 = new ivok11_IRF_Project.Design();
             this.design13 = new ivok11_IRF_Project.Design();
             this.design12 = new ivok11_IRF_Project.Design();
             this.design11 = new ivok11_IRF_Project.Design();
             this.design10 = new ivok11_IRF_Project.Design();
-            this.design9 = new ivok11_IRF_Project.Design();
             this.design8 = new ivok11_IRF_Project.Design();
             this.design7 = new ivok11_IRF_Project.Design();
             this.design6 = new ivok11_IRF_Project.Design();
             this.design5 = new ivok11_IRF_Project.Design();
+            this.design9 = new ivok11_IRF_Project.Design();
             this.design4 = new ivok11_IRF_Project.Design();
             this.design3 = new ivok11_IRF_Project.Design();
             this.design2 = new ivok11_IRF_Project.Design();
             this.design1 = new ivok11_IRF_Project.Design();
+            this.design16 = new ivok11_IRF_Project.Design();
+            this.design15 = new ivok11_IRF_Project.Design();
             this.SuspendLayout();
             // 
             // LCardsBtn
@@ -78,151 +79,162 @@ namespace ivok11_IRF_Project
             this.game1btn.UseVisualStyleBackColor = true;
             this.game1btn.Click += new System.EventHandler(this.game1btn_Click);
             // 
-            // design14
-            // 
-            this.design14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(87)))), ((int)(((byte)(227)))));
-            this.design14.Location = new System.Drawing.Point(592, 78);
-            this.design14.Name = "design14";
-            this.design14.Size = new System.Drawing.Size(31, 71);
-            this.design14.TabIndex = 26;
-            this.design14.UseVisualStyleBackColor = true;
-            // 
             // design13
             // 
-            this.design13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(62)))), ((int)(((byte)(177)))));
-            this.design13.Location = new System.Drawing.Point(161, 78);
+            this.design13.BackColor = System.Drawing.Color.Green;
+            this.design13.Location = new System.Drawing.Point(559, 242);
             this.design13.Name = "design13";
-            this.design13.Size = new System.Drawing.Size(31, 71);
-            this.design13.TabIndex = 25;
-            this.design13.UseVisualStyleBackColor = true;
+            this.design13.Size = new System.Drawing.Size(81, 37);
+            this.design13.TabIndex = 45;
+            this.design13.UseVisualStyleBackColor = false;
             // 
             // design12
             // 
-            this.design12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(62)))), ((int)(((byte)(177)))));
-            this.design12.Location = new System.Drawing.Point(357, 78);
+            this.design12.BackColor = System.Drawing.Color.Green;
+            this.design12.Location = new System.Drawing.Point(646, 242);
             this.design12.Name = "design12";
-            this.design12.Size = new System.Drawing.Size(31, 59);
-            this.design12.TabIndex = 24;
-            this.design12.UseVisualStyleBackColor = true;
+            this.design12.Size = new System.Drawing.Size(114, 37);
+            this.design12.TabIndex = 44;
+            this.design12.UseVisualStyleBackColor = false;
             // 
             // design11
             // 
-            this.design11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(140)))), ((int)(((byte)(85)))));
-            this.design11.Location = new System.Drawing.Point(34, 169);
+            this.design11.BackColor = System.Drawing.Color.Green;
+            this.design11.Location = new System.Drawing.Point(425, 242);
             this.design11.Name = "design11";
-            this.design11.Size = new System.Drawing.Size(29, 54);
-            this.design11.TabIndex = 23;
-            this.design11.UseVisualStyleBackColor = true;
+            this.design11.Size = new System.Drawing.Size(128, 37);
+            this.design11.TabIndex = 43;
+            this.design11.UseVisualStyleBackColor = false;
             // 
             // design10
             // 
-            this.design10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(218)))), ((int)(((byte)(249)))));
-            this.design10.Location = new System.Drawing.Point(34, 45);
+            this.design10.BackColor = System.Drawing.Color.Green;
+            this.design10.Location = new System.Drawing.Point(246, 242);
             this.design10.Name = "design10";
-            this.design10.Size = new System.Drawing.Size(29, 114);
-            this.design10.TabIndex = 22;
-            this.design10.UseVisualStyleBackColor = true;
-            // 
-            // design9
-            // 
-            this.design9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(218)))), ((int)(((byte)(249)))));
-            this.design9.Location = new System.Drawing.Point(69, 45);
-            this.design9.Name = "design9";
-            this.design9.Size = new System.Drawing.Size(137, 27);
-            this.design9.TabIndex = 21;
-            this.design9.UseVisualStyleBackColor = true;
+            this.design10.Size = new System.Drawing.Size(173, 37);
+            this.design10.TabIndex = 42;
+            this.design10.UseVisualStyleBackColor = false;
             // 
             // design8
             // 
-            this.design8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(218)))), ((int)(((byte)(249)))));
-            this.design8.Location = new System.Drawing.Point(212, 45);
+            this.design8.BackColor = System.Drawing.Color.Green;
+            this.design8.Location = new System.Drawing.Point(126, 242);
             this.design8.Name = "design8";
-            this.design8.Size = new System.Drawing.Size(139, 27);
-            this.design8.TabIndex = 20;
-            this.design8.UseVisualStyleBackColor = true;
+            this.design8.Size = new System.Drawing.Size(114, 37);
+            this.design8.TabIndex = 41;
+            this.design8.UseVisualStyleBackColor = false;
             // 
             // design7
             // 
-            this.design7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(115)))), ((int)(((byte)(36)))));
-            this.design7.Location = new System.Drawing.Point(357, 45);
+            this.design7.BackColor = System.Drawing.Color.Green;
+            this.design7.Location = new System.Drawing.Point(21, 242);
             this.design7.Name = "design7";
-            this.design7.Size = new System.Drawing.Size(195, 27);
-            this.design7.TabIndex = 19;
-            this.design7.UseVisualStyleBackColor = true;
+            this.design7.Size = new System.Drawing.Size(99, 37);
+            this.design7.TabIndex = 40;
+            this.design7.UseVisualStyleBackColor = false;
             // 
             // design6
             // 
-            this.design6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(115)))), ((int)(((byte)(36)))));
-            this.design6.Location = new System.Drawing.Point(558, 45);
+            this.design6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.design6.Location = new System.Drawing.Point(689, 79);
             this.design6.Name = "design6";
-            this.design6.Size = new System.Drawing.Size(159, 27);
-            this.design6.TabIndex = 18;
-            this.design6.UseVisualStyleBackColor = true;
+            this.design6.Size = new System.Drawing.Size(71, 37);
+            this.design6.TabIndex = 39;
+            this.design6.UseVisualStyleBackColor = false;
             // 
             // design5
             // 
-            this.design5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(193)))), ((int)(((byte)(199)))));
-            this.design5.Location = new System.Drawing.Point(723, 45);
+            this.design5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.design5.Location = new System.Drawing.Point(559, 79);
             this.design5.Name = "design5";
-            this.design5.Size = new System.Drawing.Size(30, 66);
-            this.design5.TabIndex = 17;
-            this.design5.UseVisualStyleBackColor = true;
+            this.design5.Size = new System.Drawing.Size(128, 37);
+            this.design5.TabIndex = 38;
+            this.design5.UseVisualStyleBackColor = false;
+            // 
+            // design9
+            // 
+            this.design9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.design9.Location = new System.Drawing.Point(722, 120);
+            this.design9.Name = "design9";
+            this.design9.Size = new System.Drawing.Size(38, 116);
+            this.design9.TabIndex = 37;
+            this.design9.UseVisualStyleBackColor = false;
             // 
             // design4
             // 
-            this.design4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(193)))), ((int)(((byte)(199)))));
-            this.design4.Location = new System.Drawing.Point(723, 117);
+            this.design4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.design4.Location = new System.Drawing.Point(21, 120);
             this.design4.Name = "design4";
-            this.design4.Size = new System.Drawing.Size(30, 106);
-            this.design4.TabIndex = 16;
-            this.design4.UseVisualStyleBackColor = true;
+            this.design4.Size = new System.Drawing.Size(38, 116);
+            this.design4.TabIndex = 32;
+            this.design4.UseVisualStyleBackColor = false;
             // 
             // design3
             // 
-            this.design3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(16)))), ((int)(((byte)(108)))));
-            this.design3.Location = new System.Drawing.Point(331, 229);
+            this.design3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.design3.Location = new System.Drawing.Point(380, 79);
             this.design3.Name = "design3";
-            this.design3.Size = new System.Drawing.Size(120, 28);
-            this.design3.TabIndex = 15;
-            this.design3.UseVisualStyleBackColor = true;
+            this.design3.Size = new System.Drawing.Size(173, 37);
+            this.design3.TabIndex = 31;
+            this.design3.UseVisualStyleBackColor = false;
             // 
             // design2
             // 
-            this.design2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(16)))), ((int)(((byte)(108)))));
-            this.design2.Location = new System.Drawing.Point(462, 229);
+            this.design2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.design2.Location = new System.Drawing.Point(141, 79);
             this.design2.Name = "design2";
-            this.design2.Size = new System.Drawing.Size(291, 28);
-            this.design2.TabIndex = 14;
-            this.design2.UseVisualStyleBackColor = true;
+            this.design2.Size = new System.Drawing.Size(99, 37);
+            this.design2.TabIndex = 30;
+            this.design2.UseVisualStyleBackColor = false;
             // 
             // design1
             // 
-            this.design1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(169)))), ((int)(((byte)(149)))));
-            this.design1.Location = new System.Drawing.Point(34, 229);
+            this.design1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.design1.Location = new System.Drawing.Point(246, 79);
             this.design1.Name = "design1";
-            this.design1.Size = new System.Drawing.Size(291, 28);
-            this.design1.TabIndex = 12;
-            this.design1.UseVisualStyleBackColor = true;
+            this.design1.Size = new System.Drawing.Size(128, 37);
+            this.design1.TabIndex = 29;
+            this.design1.UseVisualStyleBackColor = false;
+            // 
+            // design16
+            // 
+            this.design16.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.design16.Location = new System.Drawing.Point(21, 79);
+            this.design16.Name = "design16";
+            this.design16.Size = new System.Drawing.Size(114, 37);
+            this.design16.TabIndex = 28;
+            this.design16.UseVisualStyleBackColor = false;
+            // 
+            // design15
+            // 
+            this.design15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(82)))), ((int)(((byte)(104)))), ((int)(((byte)(204)))));
+            this.design15.Location = new System.Drawing.Point(44, 5);
+            this.design15.Name = "design15";
+            this.design15.Size = new System.Drawing.Size(690, 48);
+            this.design15.TabIndex = 27;
+            this.design15.Text = "Üdv a CarGames játékban, kellemes játékot és tanulást!";
+            this.design15.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 306);
-            this.Controls.Add(this.design14);
             this.Controls.Add(this.design13);
             this.Controls.Add(this.design12);
             this.Controls.Add(this.design11);
             this.Controls.Add(this.design10);
-            this.Controls.Add(this.design9);
             this.Controls.Add(this.design8);
             this.Controls.Add(this.design7);
             this.Controls.Add(this.design6);
             this.Controls.Add(this.design5);
+            this.Controls.Add(this.design9);
             this.Controls.Add(this.design4);
             this.Controls.Add(this.design3);
             this.Controls.Add(this.design2);
             this.Controls.Add(this.design1);
+            this.Controls.Add(this.design16);
+            this.Controls.Add(this.design15);
             this.Controls.Add(this.game1btn);
             this.Controls.Add(this.GuessGamebtn);
             this.Controls.Add(this.LCardsBtn);
@@ -237,20 +249,21 @@ namespace ivok11_IRF_Project
         private System.Windows.Forms.Button LCardsBtn;
         private System.Windows.Forms.Button GuessGamebtn;
         private System.Windows.Forms.Button game1btn;
+        private Design design15;
+        private Design design16;
         private Design design1;
         private Design design2;
         private Design design3;
         private Design design4;
+        private Design design9;
         private Design design5;
         private Design design6;
         private Design design7;
         private Design design8;
-        private Design design9;
         private Design design10;
         private Design design11;
         private Design design12;
         private Design design13;
-        private Design design14;
     }
 }
 
